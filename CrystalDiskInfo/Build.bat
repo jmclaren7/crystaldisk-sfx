1fa8797706594d11b811edbb34fd1448074ce818
John Mclaren <john@johnscs.com>
2024-04-16 16:13:42 -0400

    update build and run scripts

--- CrystalDiskInfo/Build.bat
+++ CrystalDiskInfo/Build.bat
@@ -2,12 +2,10 @@
 
 :: Required paths for this project
 set "inputfolder=CrystalDiskInfo"
-set "executablepath=%inputfolder%\DiskInfo64.exe"
-SET "outputexe=%inputfolder%-SFX64.exe"
+set "outputexe=%inputfolder%-SFX64.exe"
 
 :: Optional path to ResourceHacker.exe, this path will be checked first but standard paths will be checked if not found
 set "ResourceHackerPath=D:\example\Resource Hacker\ResourceHacker.exe"
-
 
 :main
 echo.
@@ -18,74 +16,29 @@ echo 2. Pack %inputfolder% to %inputfolder%.7z
 echo 3. Create %outputexe% using SFX
 echo 4. Add icon and fix manifest (requires Resource Hacker)
 echo.  
+echo F. Do steps 1,2,3,4 automatically
+echo.  
+choice /C 1234F /N /M "Build Option:"
+goto buildinput%errorlevel%
 
-
-:debloatchoice
-choice /C yna1234 /N /M "Debloat %inputfolder%? (Y/N/A=All/#=Do Only That Step):"
-goto debloatinput%errorlevel%
-:debloatinput1
-call :debloat
-call :packchoice
-call :makesfx
-call :resourcehack
-goto done
-:debloatinput2
-exit
-:debloatinput3
-call :autoall
-goto done
-:debloatinput4
+:buildinput1
 cls
 call :debloat
 goto main
-:debloatinput5
+:buildinput2
 cls
 call :pack
 goto main
-:debloatinput6
+:buildinput3
 cls
 call :makesfx
 goto main
-:debloatinput7
+:buildinput4
 cls
 call :resourcehack
 goto main
-
-
-:packchoice
-choice /C ynt /N /M "Pack %inputfolder% to %inputfolder%.7z? (T=Fast Compression) (Y/N):"
-goto packinput%errorlevel%
-:packinput1
-call :pack
-goto done
-:packinput2
-exit
-:packinput3
-call :packfast
-goto done
-
-
-:sfxchoice
-choice /C yn /N /M "Create %outputexe% using SFX? (Y/N):"
-goto sfxinput%errorlevel%
-:sfxinput1
-call :makesfx
-goto done
-:sfxinput2
-exit
-
-
-:resourcechoice
-choice /C yn /N /M "Add icon and update manifest? (Y/N):"
-goto resource%errorlevel%
-:resource1
-call :resourcehack
-goto done
-:resource2
-exit
-
-
-:autoall
+:buildinput5
+cls
 call :debloat
 call :pack
 call :makesfx
@@ -118,8 +71,7 @@ exit /B 0
 :pack
 echo.
 del /q %inputfolder%.7z
-echo %executablepath% > exe.txt
-7za64.exe a -mx=9 %inputfolder%.7z %inputfolder% run.bat exe.txt
+7za64.exe a -mx=9 %inputfolder%.7z %inputfolder% run.bat
 del /q exe.txt
 echo.
 echo Packing done, please check the output for errors before continuing.
@@ -130,8 +82,7 @@ exit /B 0
 :packfast
 echo.
 del /q %inputfolder%.7z
-echo %executablepath% > exe.txt
-7za64.exe a -mx=1 %inputfolder%.7z %inputfolder% run.bat exe.txt
+7za64.exe a -mx=1 %inputfolder%.7z %inputfolder% run.bat
 del /q exe.txt
 echo.
 echo Packing done, please check the output for errors before continuing.
